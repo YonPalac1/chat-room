58e69c66854a870f77e7f31441fab06eba0335ca
Yonpalac <yon.palac@gmail.com>
2022-10-17 20:41:28 -0300

    asd

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -46,7 +46,7 @@ const Register = () => {
 
             //create empty user chats on firestore
             await setDoc(doc(db, "userChats", res.user.uid), {});
-            navigate("/");
+            navigate("/chat-room/");
           } catch (err) {
             console.log(err);
             setErr(true);
